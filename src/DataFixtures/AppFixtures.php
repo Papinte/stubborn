4b1ecc569d82fc566e4ff8e1e23735cfff03409b
Antoine Papin <antoine.papin.dev@gmail.com>
2025-04-18 16:39:39 +0200

    Réglage de l'erreur metabase

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -11,16 +11,16 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $sweatshirts = [
-            ['name' => 'Blackbelt', 'price' => 29.90, 'isFeatured' => true, 'image' => 'blackbelt.jpg'],
-            ['name' => 'BlueBelt', 'price' => 29.90, 'isFeatured' => false, 'image' => 'bluebelt.jpg'],
-            ['name' => 'Street', 'price' => 34.50, 'isFeatured' => false, 'image' => 'street.jpg'],
-            ['name' => 'Pokeball', 'price' => 45.00, 'isFeatured' => true, 'image' => 'pokeball.jpg'],
-            ['name' => 'PinkLady', 'price' => 29.90, 'isFeatured' => false, 'image' => 'pinklady.jpg'],
-            ['name' => 'Snow', 'price' => 32.00, 'isFeatured' => false, 'image' => 'snow.jpg'],
-            ['name' => 'Greyback', 'price' => 28.50, 'isFeatured' => false, 'image' => 'greyback.jpg'],
-            ['name' => 'BlueCloud', 'price' => 45.00, 'isFeatured' => false, 'image' => 'bluecloud.jpg'],
-            ['name' => 'BornInUsa', 'price' => 59.90, 'isFeatured' => true, 'image' => 'borninusa.jpg'],
-            ['name' => 'GreenSchool', 'price' => 42.20, 'isFeatured' => false, 'image' => 'greenschool.jpg'],
+            ['name' => 'Blackbelt', 'price' => 29.90, 'isFeatured' => true, 'image' => 'blackbelt.jpeg'],
+            ['name' => 'BlueBelt', 'price' => 29.90, 'isFeatured' => false, 'image' => 'bluebelt.jepg'],
+            ['name' => 'Street', 'price' => 34.50, 'isFeatured' => false, 'image' => 'street.jepg'],
+            ['name' => 'Pokeball', 'price' => 45.00, 'isFeatured' => true, 'image' => 'pokeball.jpeg'],
+            ['name' => 'PinkLady', 'price' => 29.90, 'isFeatured' => false, 'image' => 'pinklady.jeg'],
+            ['name' => 'Snow', 'price' => 32.00, 'isFeatured' => false, 'image' => 'snow.jpeg'],
+            ['name' => 'Greyback', 'price' => 28.50, 'isFeatured' => false, 'image' => 'greyback.jpeg'],
+            ['name' => 'BlueCloud', 'price' => 45.00, 'isFeatured' => false, 'image' => 'bluecloud.jpeg'],
+            ['name' => 'BornInUsa', 'price' => 59.90, 'isFeatured' => true, 'image' => 'borninusa.jpeg'],
+            ['name' => 'GreenSchool', 'price' => 42.20, 'isFeatured' => false, 'image' => 'greenschool.jpeg'],
         ];
 
         foreach ($sweatshirts as $data) {
