04ee53fdb1f81031648ddff11a3a93277482c88e
Antoine Papin <antoine.papin.dev@gmail.com>
2025-05-02 17:11:18 +0200

    Tests réussis et lancement du server juste après

--- run-tests-and-start.bat
+++ run-tests-and-start.bat
@@ -1,10 +1,18 @@
 @echo off
-echo Running tests...
-vendor\bin\phpunit tests
-if %ERRORLEVEL% equ 0 (
-    echo Tests passed, starting server...
-    symfony server:start
+echo Lancement des tests...
+call vendor\bin\phpunit tests > nul
+echo ERRORLEVEL apres les tests : %ERRORLEVEL%
+if not errorlevel 1 (
+    echo Tests reussis, tentative de demarrage du serveur...
+    symfony server:start --no-tls --port=8000
+    if %ERRORLEVEL% neq 0 (
+        echo Echec du demarrage du serveur, verifiez que Symfony est installe et que le port 8000 est libre.
+        pause
+        exit /b %ERRORLEVEL%
+    ) else (
+        echo Serveur demarre avec succes.
+    )
 ) else (
-    echo Tests failed, server not started.
+    echo Les tests ont echoue, le serveur ne sera pas demarre.
     exit /b 1
 )
