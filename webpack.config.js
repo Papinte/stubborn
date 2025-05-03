f0cb4ce864012693ccffcf66bce9a9b231e8da17
Antoine Papin <antoine.papin.dev@gmail.com>
2025-05-03 17:38:36 +0200

    mise en place du style

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const Encore = require('@symfony/webpack-encore');
+
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+    .addEntry('app', './assets/app.js')
+    .addEntry('bootstrap', './assets/bootstrap.js') // Ajout de bootstrap.js
+    .addStyleEntry('home', './assets/styles/home/home.css')
+    .splitEntryChunks()
+    .enableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = '3.38';
+    });
+
+module.exports = Encore.getWebpackConfig();
